8eafcf48a011cead1cb11a3260c2ba2e7cb3d292
Desconocido502 <3018554470101@ingenieria.usac.edu.gt>
2023-03-26 23:33:07 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: `<h1>Hola de nuevo</h1>`
 })
 export class AppComponent {
-  title = 'bases';
+  
+
 }
